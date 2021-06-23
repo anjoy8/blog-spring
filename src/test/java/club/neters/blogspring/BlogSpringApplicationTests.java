62d49c21d03b90af1dccfd2748b784a18cbb99ef
ansonzhang <3143422472@qq.com>
2021-06-23 14:39:05 +0800

    Update BlogSpringApplicationTests.java

--- src/test/java/club/neters/blogspring/BlogSpringApplicationTests.java
+++ src/test/java/club/neters/blogspring/BlogSpringApplicationTests.java
@@ -1,8 +1,11 @@
 package club.neters.blogspring;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlogSpringApplicationTests {
-
+    @Test
+    void contextLoads() {
+    }
 }
