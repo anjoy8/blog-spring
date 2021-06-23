af6ba12489a45567b115e115b5da85ebd1a598e3
anjoy8 <3143422472@qq.com>
2021-06-23 11:45:26 +0800

    fix：code format

--- src/main/java/club/neters/blogspring/core/config/MybatisSecondConfig.java
+++ src/main/java/club/neters/blogspring/core/config/MybatisSecondConfig.java
@@ -12,7 +12,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
